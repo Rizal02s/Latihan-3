56de8b484f27bf6e3e5bc746a6bb29a163ac26c6
D-18 <D-18@D-18>
2024-06-24 20:35:28 +0700

    perpustakaan

---
+++ Perpustakaan/src/main/java/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author D-18
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Perpustakaan perpustakaan = new Perpustakaanumum();
+        perpustakaan.pinjamBuku("Buku A");
+        perpustakaan.kembalikanBuku("Buku B");
+        perpustakaan.lihatDaftarBuku();
+    }
+}
